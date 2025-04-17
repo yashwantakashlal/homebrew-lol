925df104c272ca64c084726979ca59dec1c18c0d
Yashwant <65457583+yashwantakashlal@users.noreply.github.com>
2025-04-17 14:42:32 +0530

    Update lol.rb to have new release

--- lol.rb
+++ lol.rb
@@ -1,8 +1,8 @@
 class Lol < Formula
   desc "A simple CLI tool that prints 'lol hello'"
   homepage "https://github.com/yashwantakashlal/lol"
-  url "https://github.com/yashwantakashlal/lol/releases/download/v1.0.0/lol-macos.tar.gz"
-  sha256 "a374028dfd451d6f67e9c15ef16f8efcf39e7d9262895ad5d2468ac2b396482e"
+  url "https://github.com/yashwantakashlal/lol/releases/download/v1.0.1/lol-macos-universal.tar.gz"
+  sha256 "b4e2273cc27ef83bca67290f01049bec039e8f521dd1988305aad9af724633d6"
   license "MIT"
 
   def install
